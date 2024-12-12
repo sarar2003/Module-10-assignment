b8b720a9c04109bad88807a471978affa6074e50
sarar2003 <saifurtaher40@gmail.com>
2024-12-12 08:28:55 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function calculateDifference(int a, int b) {
+    
+}
+function isOdd(int a) {
+    
+}
+function findMin(int arr[]) {
+    
+}
+function filterEvenNumber(arr1[]) {
+    if (arr1%2==0)) {
+        console.log("")
+    }
+}
+function sortArrayDescending() {
+    
+}
+function lowercaseFirstLetter(string s) {
+    
+}
+function findAverage(int arr[]) {
+    
+}
+function isLeapYear(int year){
+    if (year%2==0||) {
+        console.log(year + " is a leap year.");
+    }
+    else{
+        console.log(year + " is not a leap year.");
+    }
+}
